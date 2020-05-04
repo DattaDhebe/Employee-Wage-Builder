288afa41c5574687d4997980aaeb8596625ef832
DattaDhebe <dattadhebe75@gmail.com>
2020-05-04 15:00:15 +0000

    Refactor. small change in varibale to make code represntable

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,36 +1,50 @@
 public class EmpWageBuilder {
 	// Contants
-   public static final int IS_PART_TIME = 1;
-   public static final int IS_FULL_TIME = 2;
-   public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HOURS_IN_MONTH = 10;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	
+	private final int EMP_RATE_PER_HOUR = 20;
+	private final int NUM_OF_WORKING_DAYS = 2;
+	private final int MAX_HOURS_IN_MONTH = 10;
+	
+	
+
+	
+	public static int calcEmpWageForCompany(String Company, int empRate, int numOfDays, int maxHrs) {
+	// Variable
+      int totalEmpWage = 0, totalWorkingDays = 0;
+      int totalEmpHrs = 0, empHrs = 0; 
+      //Computation
+      while (totalEmpHrs <= maxHrs &&  totalWorkingDays> numOfDays) {
+        
+         totalWorkingDays++;
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+         switch (empCheck) {
+            case IS_FULL_TIME:
+               empHrs = 8;
+               break;
+            case IS_PART_TIME:
+               empHrs = 4;
+               break;
+            default:
+               empHrs = 0;
+         }
+      totalEmpHrs +=  empHrs;
+	System.out.println("Days#:" + totalWorkingDays + "Emp hr :" +empHrs);
+     }
+	
+      totalEmpWage = totalEmpHrs * empRate;
+      //System.out.println("For EveryDay Wage: " + empWage);
+   	System.out.println("Total employee Wage for Company : " +Company+ " is " + totalEmpWage);
+		return totalEmpWage;
+   }
+	
+	
+	
 
 	public static void main(String[] args) {
-		// Variable
-		int totalEmpWage = 0, totalWorkingDays = 0;
-		int totalEmpHrs = 0;	
-		//Computation
-		while (totalEmpHrs <= MAX_HOURS_IN_MONTH &&  totalWorkingDays < NUM_OF_WORKING_DAYS) { 
-			int empHrs = 0;
-			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-			switch (empCheck) {
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
-				default:
-					empHrs = 0;
-			}
-		totalEmpHrs +=  empHrs;
-		int empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage;
-		System.out.println("For EveryDay Wage: " + empWage);
-
-		}
-	System.out.println("Total employee Wage : " + totalEmpWage);
+		calcEmpWageForCompany("DAMrt", 20, 2, 10);
+		calcEmpWageForCompany("Reliance", 10, 4, 20);
 	}
 }
