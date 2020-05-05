f562930d2c6390f2c0d3113b97fc1cbad6cf4e8e
DattaDhebe <dattadhebe75@gmail.com>
2020-05-05 10:44:30 +0000

    Rafactor. added 'useCase7' Write class method to compute Employee

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,17 +5,26 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
-	private final int EMP_RATE_PER_HOUR = 20;
-	private final int NUM_OF_WORKING_DAYS = 2;
-	private final int MAX_HOURS_IN_MONTH = 10;
+	private final String COMPANY;
+	private final int EMP_RATE_PER_HOUR;
+	private final int NUM_OF_WORKING_DAYS;
+	private final int MAX_HOURS_IN_MONTH;
 	
-	public static void calcEmpWageForCompany(String Company, int empRate, int numOfDays, int maxHrs) {
+	public EmployeeWageBuilder(String Company, int empRate, int numOfDays, int maxHrs) {
+		this.COMPANY = Company;
+		this.EMP_RATE_PER_HOUR = empRate;
+		this.NUM_OF_WORKING_DAYS = numOfDays;
+		this.MAX_HOURS_IN_MONTH = maxHrs;
+	
+	}
+	
+	public int calcEmpWageForCompany() {
 		// Variable
       int totalEmpWage = 0, totalWorkingDays = 0;
       int totalEmpHrs = 0, empHrs = 0; 
       
-      //Dictonary to store totalWage for each Company
-      Dictionary totalEmpWageForCompany = new Hashtable();
+		//Dictonary to store totalWage for each Company
+		Dictionary<String, Integer> totalEmpWageForCompany = new Hashtable<String, Integer>();
       
       //Computation
       while (totalEmpHrs <= maxHrs &&  totalWorkingDays < numOfDays) {       
@@ -31,19 +40,20 @@ public class EmployeeWageBuilder {
             default:
                empHrs = 0;
          }
-      totalEmpHrs +=  empHrs;
+		totalEmpHrs +=  empHrs;
 		System.out.println("Days#: " + totalWorkingDays + " Emp Hrs : " +empHrs);
      }
 	
-      totalEmpWage = totalEmpHrs * empRate;
-   	totalEmpWageForCompany.put(Company, totalEmpWage);
-   	System.out.println("Total Employee Wage For Company is : "+totalEmpWageForCompany.get(Company));
+		totalEmpWage = totalEmpHrs * empRate;
+		totalEmpWageForCompany.put(Company, totalEmpWage);
+		System.out.println("Total Employee Wage For Company is : "+totalEmpWageForCompany.get(Company));
 		
    }
 	
 	public static void main(String[] args) {
-	
-		calcEmpWageForCompany("DMart", 20, 2, 10);
-		calcEmpWageForCompany("Reliance", 10, 4, 20);
+			EmployeeWageBuilder dAMrt = new EmpWageBuilder("DMart", 20, 2, 10);
+			EmployeeWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+			dAMrt.calcEmpWageForCompany();
+			reliance.calcEmpWageForCompany();
 	}
 }
